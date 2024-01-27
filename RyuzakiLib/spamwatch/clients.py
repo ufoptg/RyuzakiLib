cdd4f1e40f6eb6fd1ad3dab8c4acfa38a9d3778f
TrueSaiyan <ufperth@protonmail.com>
2024-01-27 17:57:20 +0800

    Update clients.py

--- RyuzakiLib/spamwatch/clients.py
+++ RyuzakiLib/spamwatch/clients.py
@@ -17,88 +17,86 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>
 
+import aiohttp
 from typing import List, Optional, Union
-
-import requests
 
 
 class SibylBan:
     def __init__(self, api_key: str = None):
         self.api_key = api_key
 
-    def _make_request(self, method: str, url: str, params: dict = None, json_data: dict = None):
+    async def _make_request(self, method: str, url: str, params: dict = None, json_data: dict = None):
         headers = {
             "accept": "application/json",
             "api-key": self.api_key
         }
-        try:
-            response = requests.request(
+        async with aiohttp.ClientSession() as session:
+            async with session.request(
                 method, url, headers=headers, params=params, json=json_data
-            )
-            return response.json()
-        except requests.RequestException:
-            pass
+            ) as response:
+                return await response.json()
 
-    def add_ban(self, user_id: int = None, reason: str = None, is_banned: bool = False) -> str:
+    async def add_ban(self, user_id: int = None, reason: str = None, is_banned: bool = False) -> str:
         if is_banned:
             url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/sibylban"
             payload = {"user_id": user_id, "reason": reason}
-            response = self._make_request("POST", url, json_data=payload)
+            response = await self._make_request("POST", url, json_data=payload)
             return response.get("randydev", {}).get(
                 "message", response.get("message", "Unknown error")
             )
         else:
             raise ValueError("Error: is_banned must be True")
 
-    def get_ban(self, user_id: int = None, banlist: bool = False) -> Union[dict, str]:
+    async def get_ban(self, user_id: int = None, banlist: bool = False) -> Union[dict, str]:
         if banlist:
             url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/sibyl"
             payload = {"user_id": user_id}
-            return self._make_request("GET", url, json_data=payload)
+            return await self._make_request("GET", url, json_data=payload)
         else:
             raise ValueError("Error: banlist must be True")
 
-    def unban_del(self, user_id: int = None, delete: bool = False) -> Union[dict, str]:
+    async def unban_del(self, user_id: int = None, delete: bool = False) -> Union[dict, str]:
         if delete:
             url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/sibyldel"
             payload = {"user_id": user_id}
-            return self._make_request("DELETE", url, json_data=payload)
+            return await self._make_request("DELETE", url, json_data=payload)
         else:
             raise ValueError("Error: delete must be True")
 
-    def get_all_banlist(self) -> Union[dict, str]:
+    async def get_all_banlist(self) -> Union[dict, str]:
         url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/getbanlist"
-        return self._make_request("GET", url)
-###########################################################################################################
-#UFoP SPAMWATCH
-###########################################################################################################
-    def add_ufop_ban(self, user_id: int=None, reason: str=None, is_banned: bool = False) -> str:
+        return await self._make_request("GET", url)
+    
+    ###########################################################################################################
+    # UFoP SPAMWATCH
+    ###########################################################################################################
+    async def add_ufop_ban(self, user_id: int = None, reason: str = None, is_banned: bool = False) -> str:
         if is_banned:
             url = "https://ufoptg-ufop-api.hf.space/UFoP/banner"
             payload = {"user_id": user_id, "reason": reason}
-            response = self._make_request("POST", url, json_data=payload)
+            response = await self._make_request("POST", url, json_data=payload)
             return response.get("randydev", {}).get(
                 "message", response.get("message", "Unknown error")
             )
         else:
             raise ValueError("Error: is_banned must be True")
 
-    def get_ufop_ban(self, user_id: int=None, banlist: bool = False) -> Union[dict, str]:
+    async def get_ufop_ban(self, user_id: int = None, banlist: bool = False) -> Union[dict, str]:
         if banlist:
             url = "https://ufoptg-ufop-api.hf.space/UFoP/bans"
             payload = {"user_id": user_id}
-            return self._make_request("GET", url, params=payload)
+            return await self._make_request("GET", url, params=payload)
         else:
             raise ValueError("Error: banlist must be True")
 
-    def ufopunban_del(self, user_id: int=None, delete: bool = False) -> Union[dict, str]:
+    async def ufopunban_del(self, user_id: int = None, delete: bool = False) -> Union[dict, str]:
         if delete:
             url = "https://ufoptg-ufop-api.hf.space/UFoP/bandel"
             payload = {"user_id": user_id}
-            return self._make_request("DELETE", url, json_data=payload)
+            return await self._make_request("DELETE", url, json_data=payload)
         else:
             raise ValueError("Error: delete must be True")
 
-    def get_ufop_banlist(self) -> Union[dict, str]:
+    async def get_ufop_banlist(self) -> Union[dict, str]:
         url = "https://ufoptg-ufop-api.hf.space/UFoP/getbanlist"
-        return self._make_request("GET", url)
+        return await self._make_request("GET", url)
